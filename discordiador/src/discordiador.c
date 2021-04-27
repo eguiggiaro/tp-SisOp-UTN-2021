a0fc0c206154494ef2a6e828df4170c46f25172a
Emiliano <emiliano.guiggiaro@gmail.com>
2021-04-27 20:07:25 -0300

    borrado repo eclipse
    borrado repo eclipse

--- discordiador/src/discordiador.c
+++
@@ -1,6 +0,0 @@
-#include "discordiador.h"
-
-int main(int argc, char* argv[]) {
-    puts("Hello world Soy el discordiador!!");
-    return 0;
-}
--- miram/src/miram.c
+++
@@ -1,6 +0,0 @@
-#include "miram.h"
-
-int main(int argc, char* argv[]) {
-    puts("Hello world!! Soy Mi RAM HQ");
-    return 0;
-}

a1ae2426ac5d849e1912e79342365fdf34161263
eguiggiaro <emiliano.guiggiaro@gmail.com>
2021-07-15 21:07:10 -0300

    Free elementos de la lista

--- miram/src/miram.c
+++ miram/src/miram.c
@@ -114,8 +114,8 @@ void atender_request_miram(Request *request)
 		paquete_devuelto->buffer = buffer_respuesta_expulsar;
 		enviar_paquete(paquete_devuelto, request_fd);
 		eliminar_buffer(buffer_devolucion_expulsar);
-		list_destroy(lista_mensajes);
-		list_destroy(lista);
+		list_destroy_and_destroy_elements(lista, (void*) free);
+		list_destroy_and_destroy_elements(lista_mensajes, (void*) free);
 		//eliminar_paquete(paquete_devuelto);
 		free(request);
 		pthread_mutex_unlock(&mutex_tareas);
@@ -168,8 +168,8 @@ void atender_request_miram(Request *request)
 		eliminar_buffer(buffer_devolucion_iniciar_tripulante);
 		free(posicion);
 		//eliminar_paquete(paquete_devuelto_iniciar_tripulante);
-		list_destroy(lista_mensajes);
-		list_destroy(lista);
+		list_destroy_and_destroy_elements(lista, (void*) free);
+		list_destroy_and_destroy_elements(lista_mensajes, (void*) free);
 		free(proxima_tarea);
 		free(request);
 		pthread_mutex_unlock(&mutex_tripulantes);
@@ -204,8 +204,8 @@ void atender_request_miram(Request *request)
 		enviar_paquete(paquete_devuelto, request_fd);
 		eliminar_buffer(buffer_devolucion_compactar);
 		//eliminar_paquete(paquete_devuelto);
-		list_destroy(lista_mensajes);
-		list_destroy(lista);
+		list_destroy_and_destroy_elements(lista, (void*) free);
+		list_destroy_and_destroy_elements(lista_mensajes, (void*) free);
 		free(request);
 
 		break;
@@ -247,8 +247,8 @@ void atender_request_miram(Request *request)
 		enviar_paquete(paquete_devuelto_iniciar_patota, request_fd);
 		eliminar_buffer(buffer_devolucion_iniciar_patota);
 		//eliminar_paquete(paquete_devuelto_iniciar_patota);
-		list_destroy(lista);
-		list_destroy(lista_mensajes);
+		list_destroy_and_destroy_elements(lista, (void*) free);
+		list_destroy_and_destroy_elements(lista_mensajes, (void*) free);
 		free(request);
 		pthread_mutex_unlock(&mutex_patota);
 		dump_memoria(true);
@@ -276,8 +276,8 @@ void atender_request_miram(Request *request)
 		paquete_devuelto->buffer = buffer_respuesta_tareas;
 		enviar_paquete(paquete_devuelto, request_fd);
 		eliminar_buffer(buffer_devolucion_tareas);
-		list_destroy(lista_mensajes);
-		list_destroy(lista);
+		list_destroy_and_destroy_elements(lista, (void*) free);
+		list_destroy_and_destroy_elements(lista_mensajes, (void*) free);
 		free(request);
 		pthread_mutex_unlock(&mutex_tareas);
 		break;
@@ -315,8 +315,8 @@ void atender_request_miram(Request *request)
 		paquete_devuelto->buffer = buffer_respuesta_mover;
 		enviar_paquete(paquete_devuelto, request_fd);
 		eliminar_buffer(buffer_devolucion_mover);
-		list_destroy(lista_mensajes);
-		list_destroy(lista);
+		list_destroy_and_destroy_elements(lista, (void*) free);
+		list_destroy_and_destroy_elements(lista_mensajes, (void*) free);
 		pthread_mutex_unlock(&mutex_mover);
 		break;
 
@@ -359,8 +359,8 @@ case CAMBIO_COLA:
 		paquete_devuelto->buffer = buffer_respuesta_cambiar;
 		enviar_paquete(paquete_devuelto, request_fd);
 		eliminar_buffer(buffer_devolucion_cambio_cola);
-		list_destroy(lista_mensajes);
-		list_destroy(lista);
+		list_destroy_and_destroy_elements(lista, (void*) free);
+		list_destroy_and_destroy_elements(lista_mensajes, (void*) free);
 		pthread_mutex_unlock(&mutex_mover);
 		break;
 

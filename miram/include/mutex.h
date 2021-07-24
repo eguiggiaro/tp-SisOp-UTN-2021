d91c2086ec7e0a2355cebbf092de54b0ef80ca0c
eguiggiaro <emiliano.guiggiaro@gmail.com>
2021-07-24 11:17:25 -0300

    Fix memoria

---
+++ miram/include/mutex.h
@@ -0,0 +1,10 @@
+#ifndef MUTEX_H
+#define MUTEX_H
+pthread_mutex_t mutex_tripulantes;
+pthread_mutex_t mutex_tareas;
+pthread_mutex_t mutex_expulsion;
+pthread_mutex_t mutex_patota;
+pthread_mutex_t mutex_mover;
+pthread_mutex_t mutex_cola;
+
+#endif
